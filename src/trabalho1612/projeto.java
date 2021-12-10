a23a5ee18f569f2393e93c0551f6764d1bee93d9
Interrogation6 <interrogation6@gmail.com>
2021-12-10 14:59:23 -0300

    Cabeçalho e inicializacao de matriz
    +Fiz cabeçalho
    +Criei a variavel da matriz tambem

--- src/trabalho1612/projeto.java
+++ src/trabalho1612/projeto.java
@@ -1,10 +1,25 @@
+/*------------------------------------------------------------
+	Autor: Kelvyn Dantas, Arthur Zica e Izabela Fernanda
+	
+	Objetivo do Projeto :
+
+	Data Inicial : 	10/12/2021 	14:58
+	Ult. Revisão: 	24/11/2021	20:35
+------------------------------------------------------------*/
 package trabalho1612;
 
 public class projeto {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stubaasadadasda
-
+		int matriz[][]= {
+				/*{Estoque,Custo,PreçoVenda,QuantVenda}*/
+/*Infantil Lisa*/		{0,0,0,0},
+/*Infantil Estampada*/	{0,0,0,0},
+/*Adulto Lisa*/			{0,0,0,0},
+/*Adulto Estampada*/	{0,0,0,0}};
+		//'QuantVenda' é a quantidade de produtos vendidos.
+		
+		
 	}
 
 }
