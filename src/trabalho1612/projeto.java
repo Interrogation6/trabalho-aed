d72dd36b55079fe9cb207712672d3b01cf2b8b0a
Interrogation6 <interrogation6@gmail.com>
2021-12-14 19:53:22 -0300

    implementado comando 'venda'
    +'venda' agora funciona corretamente.
    +criado uma função 'quant_query' usado pra processar quantos items foram vendidos.
    +criado 'print_inicial' pra não precisar de ficar repetindo toda hora.

--- src/trabalho1612/projeto.java
+++ src/trabalho1612/projeto.java
@@ -24,10 +24,9 @@ public class projeto {
 		
 		System.out.println("Programa iniciado.");
 		
-		
+		print_inicial();
 		while(true)
 		{
-			System.out.println("Insira seu comando(escreva 'help' para lista de comandos) :");
 			String cmd=input.nextLine();
 			
 			switch(cmd)
@@ -43,7 +42,7 @@ public class projeto {
 				encerra=1;	//Aciona variavel de encerramento.
 				break;
 			case "venda":
-				System.out.println("Qual item foi vendido?");
+				System.out.println("Qual tipo de máscara foi vendida?");
 				while(true)
 				{
 					cmd=input.nextLine();
@@ -52,17 +51,29 @@ public class projeto {
 					{
 					case "infantil lisa":
 						
+						quant_query("infantis lisas",matriz[0],input);
+						print_inicial();
+						
 						encerra=1;	//Aciona variavel de encerramento DESTA REPETICAO.
 						break;
 					case "infantil estampada":
+						
+						quant_query("infantis estampadas",matriz[1],input);
+						print_inicial();
 						
 						encerra=1;	//Aciona variavel de encerramento DESTA REPETICAO.
 						break;
 					case "adulto lisa":
 						
+						quant_query("adultas lisas",matriz[2],input);
+						print_inicial();
+						
 						encerra=1;	//Aciona variavel de encerramento DESTA REPETICAO.
 						break;
 					case "adulto estampada":
+						
+						quant_query("adultas estampadas",matriz[3],input);
+						print_inicial();
 						
 						encerra=1;	//Aciona variavel de encerramento DESTA REPETICAO.
 						break;
@@ -87,7 +98,33 @@ public class projeto {
 		input.close();
 	}
 
+	public static void quant_query(String mascara,Produto prod, Scanner input) {
+		System.out.println("Quantas máscaras "+mascara+" foram vendidas?");
+		int quant=input.nextInt();
+		if(quant<0)
+		{
+			System.out.println("Número inválido.");
+		}
+		else if(quant>prod.estoque)
+		{
+			System.out.println("Não há produtos no estoque suficientes para assumir a demanda.");
+			System.out.println("(No momento temos somente "+prod.estoque+" deste produto em estoque.)");
+		}
+		else
+		{
+			prod.estoque-=quant;
+			prod.quantvenda+=quant;
+			System.out.println(quant+" máscaras "+mascara+" vendidas com sucesso!");
+		}
+				
+	}
+	public static void print_inicial() {
+		System.out.println("");
+		System.out.println("Insira seu comando(escreva 'help' para lista de comandos) :");
+	}
 }
+
+
 
 class Produto {
 	int estoque;
